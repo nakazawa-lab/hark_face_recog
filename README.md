f2fc5bf0ffcc0dc014109787638067de1b790c64
kei15 <micomicomicotaro@yahoo.co.jp>
2019-07-20 13:53:19 +0900

    README.md書き換え

--- README.md
+++ README.md
@@ -1,19 +1,50 @@
 ## 顔認識パッケージ
 このファイルは顔認識をして認識した顔の方向を出力するパッケージです。
 
+- face_recog_to_ROS.py：顔認識をdlibで行い、カメラ座標系における顔の位置をROS通信システム上に送信します。
+- face_xyz_to_HARK.py：顔の三次元位置情報をHARKへと送信します。
+- dlib_module.py：dlibの顔認識についてまとめたファイルです。
+- getXYZ.py：pythonで三次元位置情報を扱えるかどうか確かめるためのテスト用ファイルです。
+
 ## 使用方法
-Pythonで書かれているのでROSのPythonを実行ファイルとして認識させる以下のコマンドを一度行う必要があります。
+ターミナルを4つ開いて以下をそれぞれ実行。
+
+```
+$ roscore
+```
+
+```
+$ roslaunch kinect2_bridge kinect2_bridge.launch
+```
 
 ```
 $ roscd hark_face_recog
 $ cd src
-$ chmod 755 getXYZ.py
+$ python face_recog_to_ROS.py
 ```
 
-実行コマンドは、
+```
+$ roscd hark_face_recog
+$ cd src
+$ python face_xyz_to_HARK.py
+```
+
+
+### rosrunで実行したい場合
+Pythonで書かれているのでROS上でPythonの実行ファイルとして認識させるには以下のコマンドを一度行う必要があります。
 
 ```
-$ rosrun hark_face_recog getXYZ.py
+$ roscd hark_face_recog
+$ cd src
+$ chmod 755 face_xyz_to_HARK.py face_recog_to_ROS.py
 ```
 
-です。
+## PointCloudLibraryをROSで簡単に試す場合
+
+テスト用にkinect画面上の中心点のxyzの値を取得するコードのサンプルもあります。
+iai-kinect2を立ち上げた状態で以下のコマンドで実行できます。
+```
+$ roscd hark_face_recog
+$ cd src
+$ python getXYZ.py
+```
---
+++ src/getXYZ.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+## coding: UTF-8
+
+import roslib
+import rospy
+from sensor_msgs.msg import PointCloud2
+import numpy
+import pylab
+import time
+import sensor_msgs.point_cloud2 as pc2
+
+def callback(data):
+    resolution = (data.height, data.width)
+
+    # 3D position for each pixel
+    img = numpy.fromstring(data.data, numpy.float32)
+
+    clp0 = []
+    clp1 = []
+    clp2 = []
+    for p in pc2.read_points(data, field_names = ("x", "y", "z"), skip_nans=False):
+        clp0.append(p[0])
+        clp1.append(p[1])
+        clp2.append(p[2])
+
+    x_points = numpy.array(clp0, dtype=numpy.float32)
+    y_points = numpy.array(clp1, dtype=numpy.float32)
+    z_points = numpy.array(clp2, dtype=numpy.float32)
+
+    x = x_points.reshape(resolution)
+    y = y_points.reshape(resolution)
+    z = z_points.reshape(resolution)
+
+    print ("x0: " + str(x[data.height/2 , data.width/2]) + ", y0: " + str(y[data.height/2 , data.width/2]) + ", z0: " + str(z[data.height/2 , data.width/2]))
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('/kinect2/sd/points',
+                     PointCloud2, callback)
+    rospy.spin()
+
+if __name__ == "__main__":
+    listener()
+    ## テスト
