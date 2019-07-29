13c20f1ef63bb2684358c74d876f852a97f1a29e
kei15 <micomicomicotaro@yahoo.co.jp>
2019-07-29 18:34:52 +0900

    x,y,zの取得の実装完了

--- src/face_recog_to_ROS.py
+++ src/face_recog_to_ROS.py
@@ -5,12 +5,13 @@
 import roslib
 import rospy
 from sensor_msgs.msg import Image
-from std_msgs.msg import Int64MultiArray
+from std_msgs.msg import Float32MultiArray
 from sensor_msgs.msg import CameraInfo
 
 # ros以外
 import cv2
-import numpy
+import numpy as np
+import math
 from cv_bridge import CvBridge, CvBridgeError
 import imutils
 from imutils import face_utils
@@ -24,21 +25,34 @@ class SendFaceToROS:
         self.predictor_path = "./shape_predictor_68_face_landmarks.dat"
         self.face = dm.FaceDLib(self.predictor_path)
         # ROSのメソッド
-        self._face_recog_pub = rospy.Publisher('face_recog_result', Int64MultiArray, queue_size=10)
+        self._face_recog_pub = rospy.Publisher('face_recog_result', Float32MultiArray, queue_size=10)
         self._image_sub = rospy.Subscriber('/kinect2/hd/image_color', Image, self.callback)
         # OpenCVのメソッド
         self._bridge = CvBridge()
         # kinectカメラの情報を取得
-        self.camera_info = rospy.wait_for_message("/kinect2/sd/camera_info", CameraInfo)
+        camera_info = rospy.wait_for_message("/kinect2/sd/camera_info", CameraInfo)
+        self.width = camera_info.width
+        self.height = camera_info.height
+        K = np.array(camera_info.K).reshape(3,3) # 参照：http://docs.ros.org/melodic/api/sensor_msgs/html/msg/CameraInfo.html
+        self.f = K[0][0] # 焦点距離f
 
-    def send_to_ROS(self, camera_x, camera_y):
-        array = Int64MultiArray(data=[camera_x, camera_y])
+    def send_to_ROS(self, x, y, z):
+        array = Float32MultiArray(data=[x, y, z])
         self._face_recog_pub.publish(array)
 
+    def acquire_face_angle(self, u):
+        radian = math.atan((u - self.width/2) / self.f)
+        theta = np.rad2deg(radian)
+        return theta
+
+    def theta_debug(self, theta):
+        radian = np.radians(theta)
+        u = math.tan(radian) * self.f + self.width/2
+        return u
+
     def callback(self, data):
-        kinect_width = self.camera_info.width
         cv_image = self._bridge.imgmsg_to_cv2(data, 'bgr8')
-        cv_image = imutils.resize(cv_image, kinect_width)
+        cv_image = imutils.resize(cv_image, self.width)
 
         # デバッグ用表示
         display_image = self.face.face_shape_detector_display(cv_image)
@@ -46,11 +60,17 @@ class SendFaceToROS:
         cv2.waitKey(1)
 
         face_recog_result = self.face.get_mouth_xy(cv_image)
+
         if not face_recog_result == None:
-            x = face_recog_result[0]
-            y = face_recog_result[1]
-            self.send_to_ROS(x, y)
-            print(x, y)
+            u = face_recog_result[0]
+            v = face_recog_result[1]
+            theta = self.acquire_face_angle(u)
+            print("face angle: " + str(theta) + " [degree]")
+
+            x = (u - self.width/2)
+            y = (v - self.height/2)
+            z = self.f
+            self.send_to_ROS(x, y, z)
         
 if __name__ == "__main__":
     rospy.init_node('face_recog_to_ROS',anonymous=True)
