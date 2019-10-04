93595eb36367b7d3c1bbaaf87911a9f5590169b5
kei15 <micomicomicotaro@yahoo.co.jp>
2019-10-04 15:26:25 +0900

    hark_ros_process.pyにログ出力する関数を書いた。入力のゲインもあげた。

--- src/userdata/networks/hark_ros_process.py
+++ src/userdata/networks/hark_ros_process.py
@@ -1,46 +1,32 @@
 from harkpython import harkbasenode
 import math
+import os
+import datetime
 
 class HarkNode(harkbasenode.HarkBaseNode):
     def __init__(self):
-        print("=" * 30 )
+        print("=-" * 30 )
         self.outputNames=("OUTPUT",)  # one output terminal named "output"
         #self.outputTypes=("prim_float",)  # the type is primitive float.
         self.outputTypes=("vector_source",)
         self.sources_container = []
         self.flag = 0
 
+        # EmitSourceLog関連
+        self.maxid = -1
+        path = os.getcwd()
+        self.f = open(path + "/sournd_source_log/log_" + str(datetime.datetime.now()) + ".txt", "a")
+
     def calculate(self):
 
-        if len(self.SOURCES)<1:
-            print("音源データがありません")
-        else:
-            #print(self.SOURCES[0])
-            print("sound_source:", self.SOURCES)
-
-
-        # if len(self.SOURCES2)<1:
-        #     print("顔方向データがありません")
-        #     if self.flag == 1:
-        #         self.outputValues["OUTPUT"] = self.sources_container
-        #     else:
-        #         self.outputValues["OUTPUT"] = []
-        #
-        # elif self.SOURCES2[0]['power'] == 0:
-        #     print("顔方向データがありません")
-        #     self.flag = 0
-        #     self.outputValues["OUTPUT"] = []
-        #
+        # if len(self.SOURCES)<1:
+        #     print("音源データがありません")
         # else:
-        #     #print(self.SOURCES[0])
-        #     print("image_source:", self.SOURCES2)
-        #     self.sources_container = [self.SOURCES2[0]]
-        #     self.flag = 1
-        #     self.outputValues["OUTPUT"] = self.sources_container
+        #     print("sound_source:", self.SOURCES)
 
 
         if len(self.SOURCES2)<1:
-            print("顔方向データがありません")
+            # print("顔方向データがありません")
             if self.flag == 1:
                 if len(self.SOURCES)<1:
                     self.outputValues["OUTPUT"] = self.sources_container
@@ -54,7 +40,7 @@ class HarkNode(harkbasenode.HarkBaseNode):
 
 
         elif self.SOURCES2[0]['power'] == 0:
-            print("顔方向データがありません")
+            # print("顔方向データがありません")
             self.flag = 0
 
             if len(self.SOURCES)<1:
@@ -64,7 +50,7 @@ class HarkNode(harkbasenode.HarkBaseNode):
 
         else:
             #print(self.SOURCES[0])
-            print("image_source:", self.SOURCES2)
+            # print("image_source:", self.SOURCES2)
             self.sources_container = [self.SOURCES2[0]]
             self.flag = 1
 
@@ -73,18 +59,13 @@ class HarkNode(harkbasenode.HarkBaseNode):
             else:
                 self.outputValues["OUTPUT"] = self.sources_container + self.SOURCES
 
-
-        print(self.outputValues["OUTPUT"])
-
-
-        #出力の値を設定
-        #self.outputValues["OUTPUT"] = [self.SOURCE[0]]
-        # self.outputValues["OUTPUT"] = [] if len(self.SOURCES)<1 else [self.SOURCES]
-        #self.outputValues["OUTPUT"] = [] if len(self.SOURCES2)<1 else [self.SOURCES2[0]]
-        #self.outputValues["OUTPUT"] = [] if len(self.SOURCES2)<1 or self.SOURCES2[0]['power'] == 0 else [self.SOURCES2[0]]
-
-
-
-
-        #定数入力は下記のように行う
-        #self.outputValues["OUTPUT"] = [{'x': [-0.9433000087738037, -0.1662999987602234, 0.2874000072479248], 'power': 34.90971374511719, 'id': 2}]
+        self.EmitSourceLog(self.outputValues["OUTPUT"])
+        # print(self.outputValues["OUTPUT"])
+    
+    def EmitSourceLog(self, srcs):
+        if len(srcs)==0:
+            return
+        for src in srcs:
+            if src['id'] > self.maxid:
+                self.f.write(str(src) + "\n")
+                self.maxid = src['id'] 
