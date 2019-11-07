a45ac8520eb864f6f27faaa9622c5618f991afa9
D1ngn <naganod1@gmail.com>
2019-11-07 13:14:06 +0900

    syuusei

--- src/dlib_module.py
+++ src/dlib_module.py
@@ -9,32 +9,42 @@ import sys
 import datetime
 import os
 import imutils
+import math
 from imutils import face_utils
+# from scipy.spatial import distance as dist # for desktop
 
 class FaceDLib:
 
     def __init__(self, predictor_path):
         self.predictor = dlib.shape_predictor(predictor_path)
-        self.detector = dlib.get_frontal_face_detector()
 
-    def face_shape_detector_display(self, img, img_gray):
-
-        rects = self.detector(img_gray, 0)
+    def face_shape_detector_display(self, img, img_gray, rects, mar, MAR_THRESH):
 
         for rect in rects:
-
+            # 画像の中から顔の特徴点を取得する
             shape = self.predictor(img_gray, rect)
             shape = face_utils.shape_to_np(shape)
 
-            cv2.rectangle(img, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (255, 0, 0), 1)
+            # MARの値を画面に表示する
+            cv2.putText(img, "MAR: {:.2f}".format(mar), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-            # landmarkを画像に書き込む 48:68が口。
-            for (x, y) in shape[0:68]:
-                cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+            # 口が開いている場合、画面に表示する
+            if mar > MAR_THRESH:
+                cv2.putText(img, "Mouth is Open!", (30,60),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
 
-        return img, img_gray
+            # cv2.rectangle(img, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (255, 0, 0), 1)
 
-    # 画像に映る口の座標を取得(口が動いているかを判別するために作成)
+            # landmarkを画像に書き込む 48:68が口
+            mouth_hull = cv2.convexHull(shape[48:68])
+            cv2.drawContours(img, [mouth_hull], -1, (0, 0, 255), 2)
+            # for (x, y) in shape[48:68]:
+            #     cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+
+        return img
+
+    """
+    # 画像に映る口の座標を取得(口が動いているかを判別するために作成)→現在は使っていない
     def get_mouth_xy(self, img_gray):
         x_upper = x_lower = y_upper = y_lower = None # 初期化
         rects = self.detector(img_gray, 0)
@@ -47,12 +57,32 @@ class FaceDLib:
             x_lower = mouth_point_lower.x
             y_lower = mouth_point_lower.y
         return x_upper, y_upper, x_lower, y_lower  # x, yはそれぞれlong型で、それらをタプルとして返す。
+    """
+    # mouth aspect ratio(口の開き具合の指標)を算出
+    def mouth_aspect_ratio(self, img_gray, rects):
+        mar = 0 # mouth aspect ratio初期化
+        for rect in rects:
+            shape = self.predictor(img_gray, rect)
+            shape = face_utils.shape_to_np(shape)
 
+            # 顔の各特徴点の座標を取得(Desktop)
+            # A = dist.euclidean(shape[51] , shape[59]) # 51, 59
+            # B = dist.euclidean(shape[53] , shape[57]) # 53, 57
+            # C = dist.euclidean(shape[49] , shape[55]) # 49, 55
+
+            # 顔の各特徴点の座標を取得(xavier)
+            A = np.linalg.norm(shape[51] - shape[59])
+            B = np.linalg.norm(shape[53] - shape[57])
+            C = np.linalg.norm(shape[49] - shape[55])
+
+        	# mouth aspect ratioの計算(口が開いているほど値が大きくなる)
+            mar = (A + B) / (2.0 * C)
+
+        return mar
 
     # 画像に映る鼻の座標を取得(IDを割り振る際の人物判定用に作成)
-    def get_nose_xy(self, img_gray):
+    def get_nose_xy(self, img_gray, rects):
         x = y = None # 初期化
-        rects = self.detector(img_gray, 0)
         for rect in rects:
             shape = self.predictor(img_gray, rect)
             nose_point = shape.part(30) # 鼻の点を取得
