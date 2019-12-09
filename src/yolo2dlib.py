645b498dcc3a14ab9f85c9eef3697ac948ab7428
D1ngn <naganod1@gmail.com>
2019-12-09 21:21:58 +0900

    閾値を変化量にしたりidのバグ修正

--- src/yolo2dlib.py
+++ src/yolo2dlib.py
@@ -29,7 +29,7 @@ class YOLO2Dlib:
         self.set_camera_info()
 
         self.person_bboxes = []
-        self.padding = 50
+        self.padding = 10
         self.c = 0
         self.debug_image = np.zeros((800, 800, 3))
         self.here_path = os.path.dirname(__file__)
@@ -39,11 +39,18 @@ class YOLO2Dlib:
         self.face = dm.FaceDLib(predictor_path)
         self.predictor = dlib.shape_predictor(predictor_path)
         self.detector = dlib.get_frontal_face_detector()
-        self.MAR_THRESH = 0.70
+        self.MAR_THRESH = 0.06
         self.id = 10000 # 人物判定用のid
         self.speaking_flag = 0 # 話している間１にし、話していないときは0にする
-        self.mouth_close_count = 0  # 口がどれくらいの時間閉まっているかをカウントするために用意
-        self.start_flag = 0  # 口の動きを判定し始める際の合図
+        # self.mouth_close_count = 0  # 口がどれくらいの時間閉まっているかをカウントするために用意
+        # self.start_flag = 0  # 口の動きを判定し始める際の合図
+        self.last_mar = 1
+        self.frame = 0
+        self.MOUTH_OPEN_DURATION_THRESH = 25
+        self.mouth_count = self.MOUTH_OPEN_DURATION_THRESH * (-1)
+        self.is_open_flag = False
+        self.last_is_open_flag = False
+
 
         self._bridge = CvBridge()
         self._usb_image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, self.usb_callback)
@@ -51,7 +58,7 @@ class YOLO2Dlib:
         self._dataset_image_sub = rospy.Subscriber('/moviedata_py', Image, self.dataset_callback)
         self._bboxes_sub = rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.bboxes_callback)
         self._face_recog_pub = rospy.Publisher('face_recog_result', Float32MultiArray, queue_size=10)
-        self.m_pub_threshold = rospy.get_param('~pub_threshold', 0.870)  # ROS PARAM
+        self.m_pub_threshold = rospy.get_param('~pub_threshold', 0.750)  # ROS PARAM
 
         return
 
@@ -96,6 +103,7 @@ class YOLO2Dlib:
         self.person_bboxes = person_bboxes
 
     def image_callback(self, img, mode):
+        self.is_open_flag = False
         debug_img = img
         x = 0
         y = 0
@@ -113,29 +121,14 @@ class YOLO2Dlib:
                     if len(dlib_rects) != 0:
                         mar = self.face.mouth_aspect_ratio(img_gray, dlib_rects)
                         debug_img = self.dlib_display(debug_img, img_gray, dlib_rects, mar, self.MAR_THRESH)
-                        x = -(self.nose_x - self.width/2)
-                        y = (self.nose_y - self.height/2)
-                        z = self.f
-                        id = self.id
-                        # 口が動いていないときは顔認識結果がないと判定し、0を送る
-                        is_mouth_moving = self.mouth_motion_with_mar(mar, self.speaking_flag)
-                        if not is_mouth_moving:
-                            x = 0
-                            y = 0
-                            z = 0
-                            id = 0
-                            # 話し終わったタイミングでidを1増やす
-                            if self.speaking_flag == 1:
-                                self.id += 1
-                            self.speaking_flag = 0
-                        # 顔認識結果がない場合は0を送る
-                    else:
-                        # x = 0
-                        # y = 0
-                        # z = 0
-                        # id = 0
-                        self.mouth_close_count += 1
-                        # self.send_to_ROS(x, y, z, id)
+                        self.last_mar = mar
+                        if self.is_open_flag:
+                            x = -(self.nose_x - self.width/2)
+                            y = (self.nose_y - self.height/2)
+                            z = self.f
+                            id = self.id
+        if self.last_is_open_flag and not self.is_open_flag:
+            self.id += 1
         self.send_to_ROS(x, y, z, id)
         if mode == "usb":
             debug_img = cv2.cvtColor(debug_img, cv2.COLOR_RGBA2BGR)
@@ -143,6 +136,11 @@ class YOLO2Dlib:
         cv2.namedWindow("image")
         cv2.imshow("image", debug_img)
         cv2.waitKey(1)
+        print('LAST: ' + str(self.last_is_open_flag) + ', NOW: ' + str(self.is_open_flag))
+        print(self.id)
+        self.last_is_open_flag = self.is_open_flag
+        self.frame = self.frame + 1
+
 
     def dlib_display(self, img, img_gray, rects, mar, MAR_THRESH):
         for rect in rects:
@@ -152,17 +150,25 @@ class YOLO2Dlib:
             for p in shape[48:68]:
                 p[0] = p[0] + self.left
                 p[1] = p[1] + self.upper
+            front_ratio = 100 - 100 * abs((shape[16][0]- shape[27][0]) - (shape[27][0]-shape[0][0])) / (shape[16][0]-shape[0][0])
+            cv2.putText(img, "front: " + str(front_ratio) + "%", (1500, 1000), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
             # 鼻に関して記述
             nose_p = shape[30]
             self.nose_x= nose_p[0] + self.left
             self.nose_y = nose_p[1] + self.upper
             cv2.circle(img, (self.nose_x, self.nose_y), 3, (0,0,255), thickness=5, lineType=cv2.LINE_8, shift=0)
+            dmar = mar - self.last_mar
             # MARの値を画面に表示する
-            cv2.putText(img, "MAR: {:.2f}".format(mar), (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(img, "delta MAR: {:.2f}".format(dmar), (30, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
             # 口が開いている場合、画面に表示する
-            if mar > MAR_THRESH:
-                cv2.putText(img, "Mouth is Open!", (30,60),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),2)
+            if dmar > MAR_THRESH:
+                self.mouth_count = self.frame
+            if self.frame - self.mouth_count < self.MOUTH_OPEN_DURATION_THRESH:
+                cv2.putText(img, "Mouth is Open!", (30,120),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255),2)
+                self.is_open_flag = True
+            else:
+                self.is_open_flag = False
             # landmarkを画像に書き込む 48:68が口
             mouth_hull = cv2.convexHull(shape[48:68])
             cv2.drawContours(img, [mouth_hull], -1, (0, 0, 255), 2)
@@ -185,12 +191,13 @@ class YOLO2Dlib:
                 self.upper = 0
             else:
                 self.upper = pbox.ymin - self.padding
-            self.lower = self.upper + (pbox.ymax - self.upper) / 2
-            self.left = pbox.xmin
-            self.right = pbox.xmax
+            self.lower = self.upper + int((pbox.ymax - self.upper) / 2.5)
+            self.left = pbox.xmin + (pbox.xmax - pbox.xmin) / 6
+            self.right = pbox.xmax - (pbox.xmax - pbox.xmin) / 6
             # dlibで認識できる最大横幅が72px
             if self.right - self.left > 72:
                 crop = image[self.upper:self.lower, self.left:self.right]
+                cv2.rectangle(image, (self.right, self.upper), (self.left, self.lower), (0, 0, 0), 2)
             else:
                 crop = np.asarray(crop)
         else:
@@ -198,31 +205,31 @@ class YOLO2Dlib:
         return crop
 
     # mouth_aspect_ratioを使用して口が動いているかを判定する
-    def mouth_motion_with_mar(self, mar, flag):
-
-        # 口が閉まっている場合カウントを1ずつ増やしていく
-        if mar < self.MAR_THRESH:
-            self.mouth_close_count += 1
-        # 口が開いている場合にカウントを0にする
-        else:
-            self.mouth_close_count = 0
-
-        print("mouth_close_count:", self.mouth_close_count)
-
-        # カウントが10以上の場合人が話していないと判断する
-        if self.mouth_close_count >= 10:
-            self.start_flag = 1 # 1度カウントが10を超えたらフラグを立てて(1にして)、以後はカウントが10より小さい場合に口が動いていると判定する
-            # print("話していません")
-            return False
-        else:
-            if self.start_flag == 1:
-                if self.speaking_flag == 0:
-                    self.speaking_flag = 1
-                # print("話しています")
-                return True
-            else:
-                # print("話していません")
-                return False
+    # def mouth_motion_with_mar(self, mar, flag):
+    #
+    #     # 口が閉まっている場合カウントを1ずつ増やしていく
+    #     if mar < self.MAR_THRESH:
+    #         self.mouth_close_count += 1
+    #     # 口が開いている場合にカウントを0にする
+    #     else:
+    #         self.mouth_close_count = 0
+    #
+    #     print("mouth_close_count:", self.mouth_close_count)
+    #
+    #     # カウントが10以上の場合人が話していないと判断する
+    #     if self.mouth_close_count >= 10:
+    #         self.start_flag = 1 # 1度カウントが10を超えたらフラグを立てて(1にして)、以後はカウントが10より小さい場合に口が動いていると判定する
+    #         # print("話していません")
+    #         return False
+    #     else:
+    #         if self.start_flag == 1:
+    #             if self.speaking_flag == 0:
+    #                 self.speaking_flag = 1
+    #             # print("話しています")
+    #             return True
+    #         else:
+    #             # print("話していません")
+    #             return False
 
     def send_to_ROS(self, x, y, z, id):
         array = Float32MultiArray(data=[x, y, z, id])
